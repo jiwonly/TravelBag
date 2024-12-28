5b3750e721bd960e2f51274177dfcc4a67cdfca0
jiwon <ysjjw2003@gmail.com>
2024-12-28 15:49:24 +0900

    ✨ feat: add CheckData, CheckInput, and CheckList components with checkbox functionality and SVG assets

---
+++ m7-frontend/src/components/CheckInput.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Checkbox_No from "../assets/Checkbox_No.svg";
+import CheckData_plus from "../assets/CheckData_plus.svg";
+
+export function CheckInput() {
+  return (
+    <div className="flex justify-between items-center border bg-white w-[280px] px-3 py-1.5 rounded-lg">
+      <div className="flex items-center gap-3">
+        <img
+          src={Checkbox_No} // 상태에 따라 이미지 변경
+          alt="checkbox" // 클릭 가능한 스타일
+        />
+        <p className="text-[#999] text-sm">원하는 물품을 입력하세요</p>
+      </div>
+      <button className="flex justify-center items-center rounded-md bg-white">
+        <img src={CheckData_plus} alt="delete" />
+      </button>
+    </div>
+  );
+}
---
+++ m7-frontend/src/components/CheckList.jsx
@@ -0,0 +1,15 @@
+import { CheckData } from "./CheckData";
+import { CheckInput } from "./CheckInput";
+
+export function CheckList({ title, data }) {
+  return (
+    <div className="flex flex-col bg-gray-100 py-4 px-1 rounded-md w-[300px] h-auto gap-[14px] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.12)]">
+      <p className="font-bold ml-5 text-sm">{title}</p>
+
+      <div className="flex flex-col items-center gap-[14px]">
+        <CheckData content="데이터1" />
+        <CheckInput />
+      </div>
+    </div>
+  );
+}
