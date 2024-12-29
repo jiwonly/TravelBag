01dac4db53bbf04d2620021ee256db1488fb33a6
이연서 <dlth010@naver.com>
2024-12-29 20:50:52 +0900

    💄 UI: Implement dropdown functionality in Header component

--- m7-frontend/src/components/CustomStart.jsx
+++ m7-frontend/src/components/CustomStart.jsx
@@ -2,9 +2,10 @@ import Header from "./Header";
 import { CheckList } from "./CheckList";
 
 const CustomStart = ({ icon, title }) => {
+  const isTemplate = true;
   return (
     <div>
-      <Header icon={icon} title={title} />
+      <Header isTemplate={isTemplate} icon={icon} title={title} />
       <div className="Custom px-[30px] py-[160px] flex flex-col items-start flex-[1_0_0] self-stretch rounded-b-[16px] border-[1px] bg-[var(--White,_#FFF)]">
         <div className="grid grid-cols-2 gap-[25px] w-full">
           <CheckList title="의류" />
--- m7-frontend/src/components/Header.jsx
+++ m7-frontend/src/components/Header.jsx
@@ -1,24 +1,43 @@
-const Header = ({ icon, title, memo }) => {
+import {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from "../components/ui/select";
+
+const Header = ({ isTemplate, icon, title, memo }) => {
   return (
     <div className="flex items-center py-[12px] px-[23px] gap-[10px] self-stretch border-t border-l border-r rounded-t-[16px] border-[#e5e6e8] bg-[var(--White,_#FFF)]">
       <section className="icon w-[40px] h-[40px] flex-shrink-0">
         <img src={`${icon}.png`} alt={title} />
       </section>
-      <div className="flex flex-col">
-        <section className="title text-[17px] font-[Pretendard] font-semibold not-italic leading-[28px] mb-[0px] text-[#393940]">
-          {title}
-        </section>
-        <section className="memo text-[14px] font-medium font-[Pretendard] not-italic leading-[20px] text-gray-500">
-          {memo}
-        </section>
-      </div>
-      <div>
-        <select>
-          <option>{title}</option>
-          <option>여자 혼자 여행</option>
-          <option>먹고 죽으러 가는 여행</option>
-        </select>
-      </div>
+      {isTemplate ? (
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder={title} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="eat">먹고 죽으러 가는 여행</SelectItem>
+            <SelectItem value="female">여자 혼자 여행</SelectItem>
+            <SelectItem value="male">남자 혼자 여행</SelectItem>
+          </SelectContent>
+        </Select>
+      ) : (
+        <div className="flex flex-col">
+          <section className="title text-[17px] font-[Pretendard] font-semibold not-italic leading-[28px] mb-[0px] text-[#393940]">
+            {title}
+          </section>
+          <section className="memo text-[14px] font-medium font-[Pretendard] not-italic leading-[20px] text-gray-500">
+            {memo}
+          </section>
+        </div>
+      )}
     </div>
   );
 };
