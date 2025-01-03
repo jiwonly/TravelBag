242ca8c941e52f1ba77f184ed55ce27ce902ebd2
이연서 <dlth010@naver.com>
2025-01-03 21:15:31 +0900

    ✨ feat: Add logout confirmation dialog and improve navigation logic

--- m7-frontend/src/components/SideBar.jsx
+++ m7-frontend/src/components/SideBar.jsx
@@ -16,21 +16,25 @@ import { useContext } from "react";
 import { TemplateStateContext } from "@/App";
 const items = [
   {
+    id: 0,
     title: "홈",
     url: "/",
     icon: "home",
   },
   {
+    id: 1,
     title: "챙길 것들",
     url: `/template`,
     icon: "home",
   },
   {
+    id: 2,
     title: "여행 팁",
     url: "/tip",
     icon: "home",
   },
   {
+    id: 3,
     title: "로그아웃",
     url: "/",
     icon: "home",
@@ -42,6 +46,33 @@ export function SideBar() {
   const curId =
     data.length > 0 ? Math.max(...data.map((item) => item.id)) : null;
   const nav = useNavigate();
+
+  const onLogoutClick = () => {
+    if (window.confirm("정말 로그아웃하시겠습니까?")) {
+      nav("/login");
+    }
+  };
+
+  const getLink = (id) => {
+    let url;
+    switch (id) {
+      case 0:
+        url = "/";
+        break;
+      case 1:
+        url = `template/${curId}`;
+        break;
+      case 2:
+        url = "/tip";
+        break;
+      case 3:
+        url = null;
+        break;
+      default:
+        return "/";
+    }
+    return url;
+  };
 
   return (
     <Sidebar>
@@ -61,19 +92,17 @@ export function SideBar() {
           <SidebarMenu>
             {items.map((item) => (
               <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild>
-                  <Link
-                    to={
-                      item.title === "챙길 것들"
-                        ? curId
-                          ? `${item.url}/${curId}`
-                          : "/new"
-                        : item.url
-                    }
-                  >
-                    {/* <item.icon /> */}
-                    <span>{item.title}</span>
-                  </Link>
+                <SidebarMenuButton
+                  onClick={item.id === 3 ? onLogoutClick : undefined}
+                  asChild={item.id !== 3}
+                >
+                  {item.id === 3 ? (
+                    <button className="w-full text-left">{item.title}</button> // 로그아웃 버튼
+                  ) : (
+                    <Link to={getLink(item.id)}>
+                      <span>{item.title}</span>
+                    </Link>
+                  )}
                 </SidebarMenuButton>
               </SidebarMenuItem>
             ))}
