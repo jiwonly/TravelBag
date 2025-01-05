e3a5f9baaf6264594ecf52b83ebea76c5fc8d8c2
jiwon <ysjjw2003@gmail.com>
2025-01-05 14:56:18 +0900

    🐛 fix: 로그인 백그라운드 높이 수정

--- m7-frontend/src/components/HalfTemplate.jsx
+++ m7-frontend/src/components/HalfTemplate.jsx
@@ -15,7 +15,7 @@ const HalfTemplate = ({ type, onLogin }) => {
   };
 
   return (
-    <div className="flex justify-center items-center gap-[30px] p-[28px] bg-white">
+    <div className="flex justify-center items-center gap-[30px] p-[28px] h-screen bg-white">
       <div className="flex flex-col justify-center items-center w-[490px] h-[667px] px-20 py-6  border border-[color:var(--Gray-100,#E5E6E8)] [background:var(--Gradient---Off-White,linear-gradient(180deg,#F6F6F6_0%,#FFF_100%))] rounded-2xl border-solid">
         <img src={LoginLogo} className="w-[220px] mb-[60px]" />
 
