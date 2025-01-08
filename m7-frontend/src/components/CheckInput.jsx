62235dec00fcdd6309cfd1b993c3ae53754aa46c
이연서 <dlth010@naver.com>
2025-01-09 00:50:05 +0900

    🎨 style: 체크리스트의 높이를 일정하게 조정

--- m7-frontend/src/components/CheckInput.jsx
+++ m7-frontend/src/components/CheckInput.jsx
@@ -23,7 +23,7 @@ export function CheckInput({ setListData, isEdit }) {
   };
 
   return (
-    <div className="flex justify-between items-center border bg-white w-[300px] px-3 py-1.5 rounded-lg">
+    <div className="flex justify-between items-center border bg-white w-[300px] h-[45px] px-3 py-1.5 rounded-lg">
       <div className="flex items-center gap-3">
         <img src={Checkbox_No} />
         <input
