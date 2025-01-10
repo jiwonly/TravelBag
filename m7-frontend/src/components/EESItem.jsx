95844d6fe4ac4f4f0e9497332b56968e0ccd8be3
jiwon <ysjjw2003@gmail.com>
2025-01-10 16:26:42 +0900

    ✨ feat: EESItem url 기능 추가

--- m7-frontend/src/components/EESItem.jsx
+++ m7-frontend/src/components/EESItem.jsx
@@ -16,9 +16,19 @@ const EESItem = ({ location_id, content, id, name, signature, url }) => {
       return getShopImage(location_id, id);
     }
   };
+
+  const onURL = () => {
+    if (url) {
+      window.location.href = url; // props로 받은 url로 이동
+    }
+  };
+
   return (
     <div>
-      <div className="flex justify-center w-auto h-[181px] flex-col items-center border [background:var(--Gray-50,#F5F5F6)] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.12)] p-2 rounded-xl border-solid border-[#EAEAEA] cursor-pointer">
+      <div
+        className="flex justify-center w-auto h-[181px] flex-col items-center border [background:var(--Gray-50,#F5F5F6)] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.12)] p-2 rounded-xl border-solid border-[#EAEAEA] cursor-pointer"
+        onClick={onURL}
+      >
         <div className="flex justify-center">
           <img
             className="flex justify-center items-start shrink-0 rounded-2xl"
@@ -29,9 +39,11 @@ const EESItem = ({ location_id, content, id, name, signature, url }) => {
           <div className="items-center self-stretch text-black text-center [font-family:Pretendard] text-xs font-medium leading-[13px]">
             {name}
           </div>
-          <div className="items-center self-stretch text-black text-center [font-family:Pretendard] text-xs font-medium leading-[13px]">
-            {signature}
-          </div>
+          {signature && (
+            <div className="items-center self-stretch text-black text-center [font-family:Pretendard] text-xs font-medium leading-[13px]">
+              {signature}
+            </div>
+          )}
         </div>
       </div>
     </div>
