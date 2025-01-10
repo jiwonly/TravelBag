c72f0efc6439e0199017e1b4b15e15f57a203a26
이연서 <dlth010@naver.com>
2025-01-10 21:15:11 +0900

    ✨ 물품을 추가하지 않은 상태에서 템플릿 수정 시 경고 발생

--- m7-frontend/src/components/Header.jsx
+++ m7-frontend/src/components/Header.jsx
@@ -15,8 +15,10 @@ import { useNavigate } from "react-router-dom";
 import { EditDispatchData } from "@/App";
 import { EditStateData } from "@/App";
 import { supplyStateContext } from "@/App";
+import { AddStateContext } from "@/App";
 
 const Header = ({ isTemplate, icon, id, title, memo, updateButton }) => {
+  const added = useContext(AddStateContext);
   const newSupplyList = useContext(supplyStateContext);
   const data = useContext(TemplateStateContext);
   const dispatchContext = useContext(SelectedDisplatchData);
@@ -35,7 +37,6 @@ const Header = ({ isTemplate, icon, id, title, memo, updateButton }) => {
 
   const onSetEdit = (value) => {
     setEdit(value);
-    console.log(edit);
   };
 
   const onSelected = (value) => {
@@ -49,6 +50,10 @@ const Header = ({ isTemplate, icon, id, title, memo, updateButton }) => {
       (item) => String(item.title) === String(editedTitle)
     );
     if (isEditing) {
+      if (!added) {
+        alert("물품 추가를 완료해주세요!");
+        return;
+      }
       if (existingTemplate && edit) {
         alert("이미 존재하는 템플릿입니다!");
         return;
@@ -62,6 +67,10 @@ const Header = ({ isTemplate, icon, id, title, memo, updateButton }) => {
       onEditing(true);
     }
     if (id < 4) {
+      if (!added) {
+        alert("물품 추가를 완료해주세요!");
+        return;
+      }
       if (existingTemplate) {
         alert("이미 존재하는 템플릿입니다!");
         onEditing(true);
