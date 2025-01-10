be41a512f3d5d8cc97892c39251a04fa9ebc4f60
이연서 <dlth010@naver.com>
2025-01-10 18:49:20 +0900

    🐛 RecommendAccordionList가 길어질 때 RecommendBar가 늘어나지 않는 문제 해결

--- m7-frontend/src/components/RecommendAccordionItem.jsx
+++ m7-frontend/src/components/RecommendAccordionItem.jsx
@@ -14,7 +14,7 @@ const RecommendAccordionItem = ({
   setListData,
 }) => {
   return (
-    <div className="flex w-full ">
+    <div className="flex w-full">
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
           <AccordionTrigger>{title}</AccordionTrigger>
