8503249e98d1142e2345ba9e02aabcdd74988856
이연서 <dlth010@naver.com>
2025-01-11 20:02:25 +0900

    ✨ feat: Recoil 상태(atom, selector) 생성 및 API 연동 준비

---
+++ m7-frontend/src/api/template/atom.js
@@ -0,0 +1,17 @@
+import { atom } from "recoil";
+
+// Define Recoil atoms
+export const templateState = atom({
+  key: "templateState",
+  default: [],
+});
+
+export const recommendItemState = atom({
+  key: "recommendItemState",
+  default: [],
+});
+
+export const ItemState = atom({
+  key: "ItemState",
+  default: [],
+});
