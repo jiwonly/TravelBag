1d3fad66daddd575df304f11dbb4d6762cd05334
이연서 <dlth010@naver.com>
2025-01-12 19:14:27 +0900

    ✨ feature: sideBar 토글 기능 삭제

--- m7-frontend/src/components/Dashboard.jsx
+++ m7-frontend/src/components/Dashboard.jsx
@@ -7,7 +7,7 @@ import TravelBag from "./TravelBag";
 const Dashboard = ({ icon, title, memo }) => {
   const isTemplate = false;
   return (
-    <div>
+    <div className="mt-[29px]">
       <Header isTemplate={isTemplate} icon={icon} title={title} memo={memo} />
       <div className="Main flex px-[30px] mb-[20px] flex-col items-start flex-[1_0_0] self-stretch rounded-b-[16px] border-[1px] bg-[var(--White,_#FFF)]">
         <BagList />
