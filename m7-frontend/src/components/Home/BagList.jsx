0634064801ccf10e251ee02494701f16db714b83
jiwon <ysjjw2003@gmail.com>
2025-01-17 18:33:16 +0900

    ✨ feat: oo님의 여행가방에 member의 nickname 불러옴

--- m7-frontend/src/components/Home/BagList.jsx
+++ m7-frontend/src/components/Home/BagList.jsx
@@ -1,11 +1,36 @@
 import TravelBag from "./TravelBag";
+import { getMemberAPI } from "@/api/api";
+import { useRecoilState } from "recoil";
+import { membersState } from "@/api/atom";
+import { useEffect } from "react";
 
 const BagList = () => {
+  const [members, setMembers] = useRecoilState(membersState);
+  const memberId = 1;
+
+  useEffect(() => {
+    const fetchMembers = async () => {
+      try {
+        const response = await getMemberAPI(memberId);
+        if (response) {
+          setMembers(response);
+          console.log("Member:", response);
+        } else {
+          console.error("Invalid response:", response);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchMembers();
+  }, [memberId]);
+
   return (
     <div className="baglist mt-[20px] mb-[10PX] min-h-[200px]">
       <div className="title text-[17px] text-gray-900">나의 여행가방</div>
       <div className="memo text-[13px] text-gray-500 mb-[15px]">
-        OO님의 여행가방입니다.
+        {members.nickname}님의 여행가방입니다.
       </div>
       <TravelBag />
     </div>
