4473964751e1ab656eb5687974fb11be17132abc
jiwon <ysjjw2003@gmail.com>
2025-01-20 01:34:32 +0900

    💄 UI: 버튼 높이 수정

--- m7-frontend/src/components/Home/TemplateItem.jsx
+++ m7-frontend/src/components/Home/TemplateItem.jsx
@@ -19,7 +19,7 @@ const TemplateItem = ({ id, name }) => {
       break;
   }
   const style =
-    "flex p-[6.3px] w-full h-[38px] justify-center items-center gap-[4px] self-stretch rounded-[8px] bg-[var(--Primary-500,_#24A3B6)] text-white text-[20px]";
+    "flex p-[6.3px] w-full h-[39px] justify-center items-center gap-[4px] self-stretch rounded-[8px] bg-[var(--Primary-500,_#24A3B6)] text-white text-[20px]";
   return (
     <div className="Template flex w-[240px] p-[12px] flex-col gap-[6px] rounded-[12px] border-[1px] bg-[var(--Gray-50,_#F5F5F6)] [box-shadow:0px] shadow-custom">
       <div className="title flex-[1_0_0] text-[15px] ml-[10px] font-[Pretendard] font-semibold not-italic leading-[24px] text-left self-stretch text-[#414149]">
